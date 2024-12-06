25ef20e4adb4c761c64d8d9527bf689fb924c677
ksen <XeniaAstapovich@gmail.com>
2024-12-06 13:50:48 +0300

    было выполнено дополнительное задание

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import configparser
 import git
 from datetime import datetime
 import graphviz
-import pytz #
+import pytz
+import random
 
 def load_config(config_file):
     config = configparser.ConfigParser()
@@ -18,21 +19,29 @@ def get_commits(repo_path, commit_date):
             commits.append(commit)
     return commits
 
-def build_graph(commits):
+def build_graph(commits, output_file):
     dot = graphviz.Digraph(comment='Git Commit Dependencies')
+    author_colors = {} # Словарь для хранения цветов авторов
+
     for commit in commits:
-        dot.node(commit.hexsha, f"{commit.committed_datetime.strftime('%Y-%m-%d %H:%M:%S')}\n{commit.author.name}")
+        author = commit.author.name
+        if author not in author_colors:
+            # Генерируем случайный цвет, если автора еще нет в словаре
+            author_colors[author] = "#%06x" % random.randint(0, 0xFFFFFF)
+
+        dot.node(commit.hexsha, f"{commit.committed_datetime.strftime('%Y-%m-%d %H:%M:%S')}\n{commit.author.name}", fillcolor=author_colors[author], style="filled")
         for parent in commit.parents:
             dot.edge(parent.hexsha, commit.hexsha)
-    return dot
+
+    dot.render(output_file, view=True)
+    print(dot.source)
+
 
 def main():
     config = load_config('config.ini')
-    graphviz_path = config['graphviz_path']
     repo_path = config['repo_path']
     output_file = config['output_file']
 
-    # Изменение здесь: добавляем часовой пояс
     commit_date_str = config['commit_date']
     try:
         commit_date = datetime.strptime(commit_date_str, '%Y-%m-%d').replace(tzinfo=pytz.timezone('UTC'))
@@ -44,9 +53,8 @@ def main():
     if not commits:
         print("Нет коммитов после заданной даты.")
         return
-    dot = build_graph(commits)
-    dot.render(output_file, view=True)
-    print(dot.source)
+    build_graph(commits, output_file)
+
 
 if __name__ == "__main__":
     main()
